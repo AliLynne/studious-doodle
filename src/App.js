4c0720cc4095e8601f5c1650ec3726bd4ef0f075
AliLynne <alilynne.thompson@gmail.com>
2021-01-11 12:37:17 -0800

    create List, Settings, Help components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import List from './components/List';
+import Settings from './components/Settings';
+import Help from './components/Help';
 
 import { Map } from './components/Map';
 
 function App() {
   return (
-    <div className="App">
-      <Header>
-        <h1>Header</h1>
-      </Header>
-      <main className="main">
-        <Switch>
-          <Route path="/">
-            <Map />
-          </Route>
-          <Route path="/list">{/* <List /> */}</Route>
-          <Route path="/settings">{/* <Settings /> */}</Route>
-          <Route path="/help">{/* <Help /> */}</Route>
-        </Switch>
-      </main>
-      <Footer>
-        <Navbar />
-      </Footer>
-    </div>
+    <Router>
+      <div className="App">
+        <Header>
+          <h1>Header</h1>
+        </Header>
+        <main className="main">
+          <Switch>
+            <Route path="/list">
+              <List />
+            </Route>
+            <Route path="/settings">
+              <Settings />
+            </Route>
+            <Route path="/help">
+              <Help />
+            </Route>
+            <Route path="/">
+              <Map />
+            </Route>
+          </Switch>
+        </main>
+        <Footer>
+          <Navbar />
+        </Footer>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,23 @@
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <Router>
-      <nav className="navbar" aria-label="Main Menu">
-        <ul className="navbar__list">
-          <li className="navbar__list-item">
-            <Link to="/">Map</Link>
-          </li>
-          <li className="navbar__list-item">
-            <Link to="/list">List</Link>
-          </li>
-          <li className="navbar__list-item">
-            <Link to="/settings">Settings</Link>
-          </li>
-          <li className="navbar__list-item">
-            <Link to="/help">Help</Link>
-          </li>
-        </ul>
-      </nav>
-    </Router>
+    <nav className="navbar" aria-label="Main Menu">
+      <ul className="navbar__list">
+        <li className="navbar__list-item">
+          <Link to="/">Map</Link>
+        </li>
+        <li className="navbar__list-item">
+          <Link to="/list">List</Link>
+        </li>
+        <li className="navbar__list-item">
+          <Link to="/settings">Settings</Link>
+        </li>
+        <li className="navbar__list-item">
+          <Link to="/help">Help</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
